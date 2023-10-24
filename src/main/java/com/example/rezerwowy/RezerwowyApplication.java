fc56e00bb0db5ce38c6ded87cfe263b8632eafcc
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-10-24 23:53:47 +0200

    Added basic project structure

---
+++ src/main/java/com/example/rezerwowy/RezerwowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.rezerwowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezerwowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezerwowyApplication.class, args);
+	}
+
+}
